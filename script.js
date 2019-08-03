255cdaad550c435600acfdb0bc974de81dca8778
Robert Lai <rcylai@hotmail.com>
2019-08-02 22:23:17 -0700

    Change indent and add GA tracking

--- script.js
+++ script.js
@@ -1,43 +1,53 @@
 var hasCalculated = false;
 var expectedMods = 0;
 var modCounts = {
-  mods1: 0,
-  mods2: 0,
-  mods3: 0,
+    mods1: 0,
+    mods2: 0,
+    mods3: 0,
 };
 
 function displayResult() {
-  var modScore = Object.values(modCounts).reduce(function(acc, cur) {
-    return acc
-      + Math.log(1 + cur) / Math.log(Math.sqrt(1 + expectedMods))
-      - 2 * (1 + expectedMods) / (1 + cur);
-  }, 0);
-  var resultMod = document.getElementById('result-mod');
-  resultMod.innerHTML = modScore.toFixed(4);
+    const modScore = Object.values(modCounts).reduce(function(acc, cur) {
+        return acc
+            + Math.log(1 + cur) / Math.log(Math.sqrt(1 + expectedMods))
+            - 2 * (1 + expectedMods) / (1 + cur);
+    }, 0);
+    const resultMod = document.getElementById('result-mod');
+    resultMod.innerHTML = modScore.toFixed(4);
 }
 
 function handleChange(e) {
-  var sourceName = e.target.name;
-  var value = parseInt(e.target.value);
-  e.target.value = value || '';
+    const sourceName = e.target.name;
+    const value = parseInt(e.target.value);
+    e.target.value = value || '';
 
-  if (sourceName === 'expectedMods') {
-    expectedMods = value || 0;
-  } else {
-    modCounts[sourceName] = value || 0;
-  }
+    if (sourceName === 'expectedMods') {
+        expectedMods = value || 0;
+    } else {
+        modCounts[sourceName] = value || 0;
+    }
 
-  if (hasCalculated) {
-    displayResult();
-  }
+    if (hasCalculated) {
+        displayResult();
+        trackEvent('resubmit');
+    }
 }
 
 function handleSubmit(e) {
-  e.preventDefault();
+    e.preventDefault();
 
-  var resultPanel = document.getElementById('result-panel');
-  resultPanel.classList.remove('hidden');
+    const resultPanel = document.getElementById('result-panel');
+    resultPanel.classList.remove('hidden');
 
-  hasCalculated = true;
-  displayResult();
+    hasCalculated = true;
+    displayResult();
+    trackEvent('submit');
+}
+
+function trackEvent(action) {
+    const { mods1, mods2, mods3 } = modCounts;
+    gtag('event', action, {
+        event_category: 'modscore',
+        event_label: `${expectedMods}_${mods1}_${mods2}_${mods3}`,
+    });
 }
